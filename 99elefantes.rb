6a57d94f555f68b9497ef1f183bcf2bc422e990e
codeamac04 <codeamac04@gmail.com>
2015-09-08 19:06:58 -0500

    ejercicio termindo iteraciòn de elefantes

---
+++ 99elefantes.rb
@@ -0,0 +1,14 @@
+def elephant
+	x = 0
+	
+	while x < 99
+		x += 1
+		if x ==  1
+			puts "#{x} elefante se columpiaban sobre la tela de una araña"
+		else
+			puts "#{x} elefantes se columpiaban sobre la tela de una araña"
+		end
+	end
+
+end
+elephant
